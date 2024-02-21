cd57099e7676ac784a413b05ab3509139cb60ff4
Akash Goindi <41852582+AkashGoindi@users.noreply.github.com>
2024-02-21 17:56:24 -0600

    CRUD SQL methods implemented

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -13,70 +13,70 @@ exports.contacts_page = function(req, res, next) {
   res.render('contacts', {pageTitle: "All Contacts", contacts: data});
 }
 
-// exports.details_page = function(req, res, next) {
-//   const contactId = req.params.id;
-//   const contact = contactsRepo.findById(contactId);
-//   if (contact) {
-//     res.render('contact', { pageTitle: "Contact Details", data: contact });
-//   } else {
-//     res.redirect('/contacts');
-//   }
-// }
+exports.details_page = function(req, res, next) {
+  const contactId = req.params.id;
+  const contact = contactsRepo.findById(contactId);
+  if (contact) {
+    res.render('contact', { pageTitle: "Contact Details", data: contact });
+  } else {
+    res.redirect('/contacts');
+  }
+}
 
 // /* Create */
-// exports.add_contact_page = function(req, res, next) {
-//   res.render('contact_add', {pageTitle: "Add New Contact", errors: null});
-// }
+exports.add_contact_page = function(req, res, next) {
+  res.render('contact_add', {pageTitle: "Add New Contact", errors: null});
+}
 
-// exports.create_contact = function (req, res, next) {
-//   const result = validationResult(req);
-//   if (!result.isEmpty()) {
-//     res.render('contact_add', { pageTitle: "Add New Contact", errors: result.array() });
-//   } else {
-//     let fname = req.body.firstName.trim();
-//     let lname = req.body.lastName.trim();
-//     let email = req.body.email ? req.body.email.trim() : "";
-//     let notes = req.body.notes ? req.body.notes.trim() : "";
-//     contactsRepo.create(new Contact('', fname, lname, email, notes));
-//     res.redirect('/contacts');
-//   }
-// };
+exports.create_contact = function (req, res, next) {
+  const result = validationResult(req);
+  if (!result.isEmpty()) {
+    res.render('contact_add', { pageTitle: "Add New Contact", errors: result.array() });
+  } else {
+    let fname = req.body.firstName.trim();
+    let lname = req.body.lastName.trim();
+    let email = req.body.email ? req.body.email.trim() : "";
+    let notes = req.body.notes ? req.body.notes.trim() : "";
+    contactsRepo.create(new Contact('', fname, lname, email, notes));
+    res.redirect('/contacts');
+  }
+};
 
 // /* Update */
-// exports.edit_contact_page = function (req, res, next) {
-//   const contactId = req.params.id;
-//   const contact = contactsRepo.findById(contactId);
-//   res.render('contact_edit', {pageTitle: `Edit ${contact.getName()}'s Contact`, errors: null, data: contact});
-// };
+exports.edit_contact_page = function (req, res, next) {
+  const contactId = req.params.id;
+  const contact = contactsRepo.findById(contactId);
+  res.render('contact_edit', {pageTitle: `Edit ${contact.getName()}'s Contact`, errors: null, data: contact});
+};
 
-// exports.edit_contact = function (req, res, next) {
-//   const result = validationResult(req);
-//   const contactId = req.params.id;
-//   const contact = contactsRepo.findById(contactId);
-//   if (!result.isEmpty()) {
-//     res.render(`contact_edit`, { pageTitle: "Edit Contact", errors: result.array(), data: contact });
-//   } else {
-//     let fname = req.body.firstName.trim();
-//     let lname = req.body.lastName.trim();
-//     let email = req.body.email ? req.body.email.trim() : "";
-//     let notes = req.body.notes ? req.body.notes.trim() : "";
+exports.edit_contact = function (req, res, next) {
+  const result = validationResult(req);
+  const contactId = req.params.id;
+  const contact = contactsRepo.findById(contactId);
+  if (!result.isEmpty()) {
+    res.render(`contact_edit`, { pageTitle: "Edit Contact", errors: result.array(), data: contact });
+  } else {
+    let fname = req.body.firstName.trim();
+    let lname = req.body.lastName.trim();
+    let email = req.body.email ? req.body.email.trim() : "";
+    let notes = req.body.notes ? req.body.notes.trim() : "";
 
-//     contactsRepo.update(new Contact(contactId, fname, lname, email, notes));
+    contactsRepo.update(new Contact(contactId, fname, lname, email, notes));
 
-//     res.redirect('/contacts');
-//   }
-// };
+    res.redirect('/contacts');
+  }
+};
 
 
 // /* Delete */
-// exports.delete_contact_page = function (req, res, next) {
-//   const contactId = req.params.id;
-//   res.render('contact_delete', {id: contactId});
-// };
+exports.delete_contact_page = function (req, res, next) {
+  const contactId = req.params.id;
+  res.render('contact_delete', {id: contactId});
+};
 
-// exports.delete_contact = function (req, res, next) {
-//   const contactId = req.params.id;
-//   contactsRepo.deleteById(contactId);
-//   res.redirect('/contacts');
-// };
+exports.delete_contact = function (req, res, next) {
+  const contactId = req.params.id;
+  contactsRepo.deleteById(contactId);
+  res.redirect('/contacts');
+};
 
--- src/contactSQLiteRepo.js
+++ src/contactSQLiteRepo.js
@@ -18,26 +18,28 @@ const repo = {
         });
         return contacts;
     },
-    // findById: (uuid) => {
-    //     const stmt = db.prepare("SELECT * FROM contacts WHERE id = ?");
-    //     const row = stmt.get(uuid);
-    //     return new Contact(row.id, row.first_name, row.last_name, row.email, row.notes, row.text);
-    // },
-    // create: (contact) => {
-    //     const stmt = db.prepare("INSERT INTO contacts (text) VALUES (?)");
-    //     const info = stmt.run(todo.text);
-    //     console.log(`Contact created with id: ${info.lastInsertRowid}`);
-    // },
-    // deleteById: (uuid) => {
-    //     const stmt = db.prepare("DELETE FROM contacts WHERE id = ?");
-    //     const info = stmt.run(uuid);
-    //     console.log(`Rows affected: ${info.changes}`);
-    // },
-    // update: (contact) => {
-    //     const stmt = db.prepare("UPDATE todos SET text = ? WHERE id = ?");
-    //     const info = stmt.run(contact.text, contact.id);
-    //     console.log(`Rows affected: ${info.changes}`);
-    // },
+    findById: (id) => {
+        const stmt = db.prepare("SELECT * FROM contacts WHERE id = ?");
+        const row = stmt.get(id);
+        return new Contact(row.id, row.first_name, row.last_name, row.email, row.notes, row.text);
+    },
+    create: (contact) => {
+        const stmt = db.prepare("INSERT INTO contacts (first_name, last_name, email, notes, date) VALUES (?, ?, ?, ?, ?)");
+        const {firstName, lastName, email, notes, date} = contact;
+        const info = stmt.run(firstName, lastName, email, notes, date);
+        console.log(`Contact created with id: ${info.lastInsertRowid}`);
+    },
+    deleteById: (id) => {
+        const stmt = db.prepare("DELETE FROM contacts WHERE id = ?");
+        const info = stmt.run(id);
+        console.log(`Rows affected: ${info.changes}`);
+    },
+    update: (contact) => {
+        const stmt = db.prepare("UPDATE contacts SET first_name = ?, last_name = ?, email = ?, notes = ?, date = ? WHERE id = ?");
+        const {firstName, lastName, email, notes, date, id} = contact;
+        const info = stmt.run(firstName, lastName, email, notes, date, id);
+        console.log(`Rows affected: ${info.changes}`);
+    },
 
 };
 
