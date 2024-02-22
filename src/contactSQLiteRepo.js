91bfdbfcf7bc37dbf0fc7b108bfd1e59863ed069
Akash Goindi <41852582+AkashGoindi@users.noreply.github.com>
2024-02-21 21:43:18 -0600

    refactoring

--- src/contactSQLiteRepo.js
+++ src/contactSQLiteRepo.js
@@ -2,7 +2,7 @@ const path = require('node:path');
 const Contact = require('../src/Contact');
 const betterSqlite3 = require('better-sqlite3');
 
-const db = new betterSqlite3(path.join(__dirname, '../data/contacts.sqlite'), { verbose: console.log });
+const db = new betterSqlite3(path.join(__dirname, '../database/contacts.sqlite'), { verbose: console.log });
 
 const createStmt = db.prepare("CREATE TABLE IF NOT EXISTS contacts (id INTEGER PRIMARY KEY AUTOINCREMENT, first_name TEXT, last_name TEXT, email TEXT, notes TEXT, date TEXT)");
 createStmt.run();
